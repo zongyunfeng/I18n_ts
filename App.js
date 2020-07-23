0dcc0e5d521700c4abdaf2785b19406472376cc7
宗云峰 <zongyunfeng@zongyunfengdeiMac.local>
2020-07-23 11:11:55 +0800

    react to locale change event

--- App.js
+++ App.js
@@ -10,14 +10,21 @@ import React from 'react';
 import i18n from './i18n';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import * as RNLocalize from 'react-native-localize';
+import {useI18n} from '@react-native-localize-ext/react';
 
-const App = () => {
+function handleLocalizationChange() {
   const locales = RNLocalize.getLocales();
   const languageCode = locales[0].languageCode;
   console.log(languageCode);
   i18n._.locale(languageCode);
+}
+
+const App = () => {
+  handleLocalizationChange();
+  RNLocalize.addEventListener('change', handleLocalizationChange);
   const profile = i18n.profile.info();
   const time = i18n.today();
+  useI18n(i18n);
   return (
     <View style={styles.container}>
       <Text>{profile}</Text>
